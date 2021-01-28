93ddda0b12445b48ec924eebfcc9c8e80611cbcd
Paulo-AS <paulo.as@outlook.com.br>
2021-01-28 15:19:12 -0300

    Criada a interface com o DialogFlow

---
+++ dialogflow.js
@@ -0,0 +1,28 @@
+const diealogflow = require('dialogflow');
+const configs = require('./dio-bot-fit');
+
+const sessionClient = new diealogflow.SessionsClient({
+    projectId: configs.project_id,
+    credentials: {
+        private_key: configs.private_key,
+        client_email: configs.client_email
+    }
+});
+
+async function sendMessage(chatId, message) {
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode: 'pt-BR'
+            }
+        }
+    }
+
+    const response = await sessionClient.detectIntent(request);
+    const result = responses[0].queryResult;
+    console.log(JSON.stringify(result, null, 2));
+}
+
+sendMessage('830838672', 'oi');
